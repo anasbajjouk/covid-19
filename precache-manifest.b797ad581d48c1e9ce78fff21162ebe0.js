8e3951b651ee1adca0ea123cf4bed0b91b291d94
anasbajjouk <a.bajjouk@cleo.one>
2020-05-19 22:57:51 +0200

    Updates

---
+++ precache-manifest.b797ad581d48c1e9ce78fff21162ebe0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a879f59c2493ebb6540f246b3d07d661",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "185e74272e512bb257b0",
+    "url": "/covid-19/static/css/main.23ad5226.chunk.css"
+  },
+  {
+    "revision": "7a6fa2d86d630f0f3f68",
+    "url": "/covid-19/static/js/2.7973b184.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19/static/js/2.7973b184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "185e74272e512bb257b0",
+    "url": "/covid-19/static/js/main.b53c1b61.chunk.js"
+  },
+  {
+    "revision": "c75e8770ac86d7f9549a",
+    "url": "/covid-19/static/js/runtime-main.86bf5399.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19/static/media/image.d7265326.png"
+  }
+]);
